260791ce4859f4084d571d1cd10428415d291296
Everaldo Junior <jr.o2@hotmail.com>
2017-06-25 19:00:57 -0300

    Olá Mundo

---
+++ 0_ola_mundo/0_ola_mundo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_ola_mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            System.Threading.Thread.Sleep(5 * 1000);//Espera 5 segundos.
+        }
+    }
+}
