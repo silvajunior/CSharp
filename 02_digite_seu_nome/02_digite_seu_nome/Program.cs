4a7e4de04e745b17dad0f1452f5a0678a079fefd
Everaldo Junior <jr.o2@hotmail.com>
2017-06-26 22:50:57 -0300

    Digite seu nome

---
+++ 02_digite_seu_nome/02_digite_seu_nome/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_digite_seu_nome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Seja bem-vindo " +nome+" !");
+            System.Threading.Thread.Sleep(5 * 1000);//Espera 5 segundos.
+        }
+    }
+}
