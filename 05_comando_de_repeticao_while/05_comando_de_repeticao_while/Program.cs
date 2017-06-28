16e1e63e29304f4db31048fb5f97b45620660431
Everaldo Junior <jr.o2@hotmail.com>
2017-06-28 14:33:55 -0300

    Comando de repetição while

---
+++ 05_comando_de_repeticao_while/05_comando_de_repeticao_while/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_comando_de_repeticao_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0;
+            while (i<10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            System.Threading.Thread.Sleep(5 * 1000);//Espera 5 segundos.
+        }
+    }
+}
