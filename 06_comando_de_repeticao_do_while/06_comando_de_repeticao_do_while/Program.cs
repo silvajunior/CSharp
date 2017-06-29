43d7959102f88258a9b3e7f7f0cf79325f8eec46
Everaldo Junior <jr.o2@hotmail.com>
2017-06-29 08:39:06 -0300

    Comando de repetição do while

---
+++ 06_comando_de_repeticao_do_while/06_comando_de_repeticao_do_while/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_comando_de_repeticao_do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string op;
+            do
+            {
+                Console.WriteLine("Deseja executar novamente sim(Qualquer tecla) não(0): ");
+                op = Console.ReadLine();
+            } while (op != "0");
+            
+        }
+    }
+}
